7beed4697cc3ab454c05b99299b90032953c8aa6
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2022-06-03 14:35:42 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("exercicio calculadora");
+        Calculadora.soma(5,6);
+        Calculadora.subtracao(4,3);
+        Calculadora.multiplicacao(4,4);
+        Calculadora.divisao(10,2);
+
+        
+        
+    }
+}
